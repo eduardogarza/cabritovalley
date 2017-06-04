4001a2ac7da8b8c350109f68643b3ecc253aa234
eduardogarza <laloalejandro@gmail.com>
2017-06-04 05:47:23 -0500

    3

--- js/grid.js
+++ js/grid.js
@@ -344,10 +344,9 @@ var Grid = (function() {
 			// create Preview structure:
 			this.$title = $( '<h3></h3>' );
 			this.$description = $( '<p></p>' );
-			this.$twitter = $('<a href="" class="social"><i class="fa fa-twitter"></i></a>');
-			this.$facebook = $('<a href="" class="social"><i class="fa fa-facebook"></i></a>');
+			
 			this.$href = $( '<a href="#">Visit website</a>' );
-			this.$details = $( '<div class="og-details"></div>' ).append( this.$title, this.$description, this.$twitter, this.$facebook);
+			this.$details = $( '<div class="og-details"></div>' ).append( this.$title, this.$description);
 			this.$loading = $( '<div class="og-loading"></div>' );
 			this.$fullimage = $( '<div class="og-fullimg"></div>' ).append( this.$loading );
 			this.$closePreview = $( '<span class="og-close"></span>' );
@@ -393,8 +392,7 @@ var Grid = (function() {
 
 			this.$title.html( eldata.title );
 			this.$description.html( eldata.description );
-			this.$twitter.attr('href', 'https://twitter.com/'+eldata.twitter);
-			this.$facebook.attr('href', 'https://www.facebook.com/'+eldata.facebook);
+			
 			this.$href.attr( 'href', eldata.href );
 
 			var self = this;
