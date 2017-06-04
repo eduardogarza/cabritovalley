956e42837da48d32a181d5a6a2835b2ad4ec6cd0
eduardogarza <laloalejandro@gmail.com>
2017-06-04 06:36:34 -0500

    no animations

--- js/main.js
+++ js/main.js
@@ -98,29 +98,7 @@ function inviewAnimation() {
 
 	})
 	
-	$('.heading > h1').one('inview', function(event, visible){
-		$(this).addClass('animate fadeInRight');
-	});
 	
-	$('.heading > div').one('inview', function(event, visible){
-		$(this).addClass('animate fadeInLeft');
-	});
-	
-	$('.process-item').one('inview', function(event, visible){
-		$(this).addClass('animate fadeInLeft');
-	});
-	
-	$('.service-item').one('inview', function(event, visible){
-		$(this).addClass('animate');
-	})
-	
-	$('.adress-element').one('inview', function(event, visible){
-		$(this).addClass('animate fadeInUp');
-	})
-	
-	$('.about-item').one('inview', function(event, visible){
-		$(this).addClass('animate fadeInUp');
-	})
 }
 
 function filterPath(string) {
